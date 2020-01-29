b7d52cb32590a2eb304d7cfc6f88ccb217ded420
avinashmunnangi <avinashsaipavanmunnangi@gmail.com>
2020-01-29 11:14:57 +0530

    Added Logo Image

---
+++ components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Logo.css";
+const logo = props => (
+  <div className="Logo">
+    <img
+      src="https://github.com/avinashmunnangi/burgerapp1/blob/addingnewThings/assets/burger-logo.png?raw=true"
+      alt="logo"
+    />
+  </div>
+);
+
+export default logo;
